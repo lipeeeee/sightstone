237ec19e0473960c4f9bae5659e35ba9d1ef6c59
lipeeeee <filipe.pereirafga@gmail.com>
2024-01-13 20:01:36 +0000

    New info label on bottom saying the status of lca connection

--- sightstone/gui.py
+++ sightstone/gui.py
@@ -21,12 +21,18 @@ def init_gui(sightstone_hook: Sightstone):
             with dpg.tab(label="Client"):
                 dpg.add_button(label="Remove Challenges",
                                callback=sightstone_hook.remove_challenges)
+            with dpg.tab(label="Queue"):
+                with dpg.table(header_row=False):
+                    dpg.add_table_column()
+        with dpg.colormap_registry(label="Colormap Registry", show=False):
+            dpg.add_colormap([[255, 255, 255, 255], [255, 255, 255, 255]], True, tag="white", label="white")
+        dpg.add_text("INFO TEXT", tag="info", pos=(WIDTH // 2 - 80, HEIGHT - 75))
+
     dpg.set_primary_window("p1", True)
 
     # safe title for riot detection sake
     letter_dict = string.ascii_letters + string.digits + string.punctuation
     safe_title = "".join(random.choice(letter_dict) for _ in range(20))
-
     dpg.create_viewport(
         title=safe_title,
         width=WIDTH, height=HEIGHT,
@@ -36,5 +42,9 @@ def init_gui(sightstone_hook: Sightstone):
 
     dpg.setup_dearpygui()
     while dpg.is_dearpygui_running():
+        # Update necessary info for each frame
+        connected_string = "CONNECTED TO CLIENT" if sightstone_hook.lca_hook.connected else "NOT CONNECTED TO CLIENT"
+        dpg.set_value("info", connected_string)
+
         dpg.render_dearpygui_frame()
     dpg.destroy_context()
